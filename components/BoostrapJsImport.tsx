5518b1afb4b7bd080c2230eeca89ad183863c217
ramses1998 <ramsestetang2017@outlook.com>
2023-11-27 22:57:59 +0100

    Scaffolded project + Dependency Installation

---
+++ components/BoostrapJsImport.tsx
@@ -0,0 +1,9 @@
+"use client"
+import {FunctionComponent, useEffect} from "react";
+
+export const BoostrapJsImport: FunctionComponent = () => {
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }, []);
+    return null;
+}
