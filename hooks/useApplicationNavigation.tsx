75d6bafd3b92bc810335626825b5c045790021de
ramses1998 <ramsestetang2017@outlook.com>
2023-12-04 18:08:20 +0100

    Startseite made responsive for different screen sizes

--- hooks/useApplicationNavigation.tsx
+++ hooks/useApplicationNavigation.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowRightToBracket, faBook, faHouse, faPlus} from "@fortawesome/free-solid-svg-icons";
 
